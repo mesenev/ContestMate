a9c48b12ca1ac85f9dabb0533c2227cdc4f02471
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2021-04-03 18:56:13 +1000

    FINAL SUPER VERSION

--- src/main/java/org/intellij/sdk/toolWindow/ContentMate.java
+++ src/main/java/org/intellij/sdk/toolWindow/ContentMate.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -32,37 +33,23 @@ import java.util.List;
 
 public class ContentMate {
 
-
-    //
-    //System.out.println(settings.script1_makeJson_settings);
-
     private JPanel myToolWindowContent;
     private JComboBox comboBox1;
     private JTextField textField1;
     private JTextField textField2;
     private JButton loadButton;
+
     private static Integer submitID;
+    public static String pathtofile;
+
     public AppSettingsState settings = AppSettingsState.getInstance();
     public JTable table1;
+
     public List<Submit> data_submits = new ArrayList<>();
     public StringBuilder jsonString = new StringBuilder();
+
     String line;
     SubmitTableModel model = new SubmitTableModel(data_submits);
-
-    public static Path downloadFile(String urlString, Path downloadDirectory, String name, String username) throws IOException {
-        URL url = new URL(urlString);
-        String filename = name;
-        String suffix = username;
-        InputStream inputStream = url.openStream();
-        Path tempFile = Files.createTempFile(filename, suffix);
-        Path target = Paths.get(downloadDirectory + "/" + filename + suffix);
-        if (!target.toFile().exists()) {
-            Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
-            Files.move(tempFile, target);
-        }
-        return target;
-    }
-
 
     public ContentMate(ToolWindow toolWindow) throws IOException {
         table1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -72,10 +59,7 @@ public class ContentMate {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 submitID = model.data.get(table1.getSelectedRow()).id_submit;
-                String pathtofile = "C:\\Users\\mrrla\\IdeaProjects\\ContestMate\\src\\main\\files\\"
-                        + model.data.get(table1.getSelectedRow()).id_submit
-                        + model.data.get(table1.getSelectedRow()).id_problem;
-                @Nullable VirtualFile file = LocalFileSystem.getInstance().findFileByPath(pathtofile);
+                @Nullable VirtualFile file = LocalFileSystem.getInstance().findFileByPath(model.data.get(table1.getSelectedRow()).pathfile);
                 FileEditorManager.getInstance(ProjectManager.getInstance().getOpenProjects()[0]).openFile(file, true);
             }
         });
@@ -95,25 +79,55 @@ public class ContentMate {
         });
     }
 
+    public static Path downloadFile(String urlString, Path downloadDirectory, String name) throws IOException {
+        String filename = name;
+        URL url = new URL(urlString);
+        URL obj = new URL(urlString);
+        InputStream inputStream = url.openStream();
+        URLConnection conn = obj.openConnection();
+
+        String server = conn.getHeaderField("Content-Disposition");
+        String extention = server.substring(server.lastIndexOf("."));
+
+        Path tempFile = Files.createTempFile(filename, extention);
+        Path target = Paths.get(downloadDirectory + "/" + filename + extention);
+
+        if (!target.toFile().exists()) {
+            Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
+            Files.move(tempFile, target);
+        }
+        return target;
+    }
+
     public static Integer SubmitId() {
         return submitID;
     }
 
     public void make_json() throws IOException {
+
         data_submits.clear();
         jsonString.setLength(0);
+        pathtofile = "";
+
         Process executable = Runtime.getRuntime().exec(settings.script1_makeJson_settings + settings.sid + settings.tournament);
         BufferedReader is = new BufferedReader(new InputStreamReader(executable.getInputStream()));
 
         while ((line = is.readLine()) != null) {
             jsonString.append(line);
         }
+
         JSONObject obj = new JSONObject(jsonString.toString());
         JSONArray arr = obj.getJSONArray("submit");
 
         for (int i = 0; i < arr.length(); i++) {
+            pathtofile = "";
+            pathtofile = downloadFile(arr.getJSONObject(i).getString("content_link"),
+                    Paths.get("C:\\Users\\mrrla\\IdeaProjects\\ContestMate\\src\\main\\files"),
+                    String.valueOf(arr.getJSONObject(i).getInt("id_submit"))).toString();
+
             Submit a = new Submit(
                     arr.getJSONObject(i).getInt("id_problem"),
+                    pathtofile,
                     arr.getJSONObject(i).getString("problem_name"),
                     arr.getJSONObject(i).getInt("id_submit"),
                     arr.getJSONObject(i).getString("username"),
@@ -121,9 +135,7 @@ public class ContentMate {
                     arr.getJSONObject(i).getString("current_status")
             );
             data_submits.add(a);
-            downloadFile(a.content_link, Paths.get("C:\\Users\\mrrla\\IdeaProjects\\ContestMate\\src\\main\\files"), String.valueOf(a.id_submit), String.valueOf(a.id_problem));
         }
-
         executable.destroy();
         is.close();
     }
--- src/main/java/org/intellij/sdk/toolWindow/Submit.java
+++ src/main/java/org/intellij/sdk/toolWindow/Submit.java
@@ -8,13 +8,15 @@ public class Submit {
     public String username;
     public String content_link;
     public String current_status;
+    public String pathfile;
 
-    Submit(int id_problem, String problem_name, int id_submit, String username, String content_link, String current_status) {
+    Submit(int id_problem, String pathfile, String problem_name, int id_submit, String username, String content_link, String current_status) {
         this.id_problem = id_problem;
         this.problem_name = problem_name;
         this.id_submit = id_submit;
         this.username = username;
         this.content_link = content_link;
         this.current_status = current_status;
+        this.pathfile = pathfile;
     }
 }
